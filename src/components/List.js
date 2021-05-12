1b23504d34f21a71a1d71f853b72c28ea1441132
Andrew Rusli <andreworusli@gmail.com>
2021-05-12 14:53:10 -0400

    added media query/responsive layout early version

--- src/components/List.js
+++ src/components/List.js
@@ -6,14 +6,12 @@ const List = () => {
 
         <div className='bg-image centered-container' style={{backgroundImage: `url(${list})`, }}>
             <div className='inner-container centered-container'>
-                <a target="_blank" href="https://arusli-listmaker.netlify.app/"><div className='list'>
-                    <h2 className='list-header'>List Maker</h2>
+                <a target="_blank" href="https://arusli-listmaker.netlify.app/"><div className='project-square list'>
+                    <h2 className='project-header'>List Maker</h2>
                     <div className='inner-div'>
                         <p>A simple, fullstack, CRUD web app for making lists.</p>
-                        <p>Built with React/node/express/mysql.</p>  
-                        <p>Mobile-responsive.</p>
-                        <p>Integrated with Google Login.</p>  
-                        <p>Deployed via Heroku and Netlify.</p>
+                        <p>React/node/express/mysql.</p>  
+                        <p>Mobile-responsive, integrated with Google Login.</p>  
                     </div>
                 </div></a>  
             </div>
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -5,11 +5,11 @@ const Tetris = () => {
     return (
         <div className='bg-image centered-container' style={{backgroundImage: `url(${tetris})`, }}>
             <div className='inner-container centered-container'>
-                <a target="_blank" href="https://miamitetris.herokuapp.com/"><div className='tetris'>
-                    <h2 className='tetris-header'>Miami Tetris</h2>
+                <a target="_blank" href="https://miamitetris.herokuapp.com/"><div className='project-square tetris'>
+                    <h2 className='project-header'>Miami Tetris</h2>
                     <div className='inner-div'>
                         <p>A miami-themed version of the classic game.</p>
-                        <p>Built with javascript/html/css.</p>
+                        <p>Javascript/html/css.</p>
                     </div>
                 </div></a>
                 
