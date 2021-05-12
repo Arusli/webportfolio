642868a72c882f93d47fa1bf5b9711053dafc40d
Andrew Rusli <andreworusli@gmail.com>
2021-05-12 19:59:29 -0400

    add wireframed contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,22 @@ import React from 'react';
 const Contact = () => {
 
     return (
-        <div>Contact</div>
+        <div className='bg-color centered-container'>
+        <div className='contact'>
+            <h2>Contact</h2>
+            <div className='flex-contact'>
+                <div className='sub-contact'>
+                    <a>Github</a>
+                </div>
+                <div className='sub-contact'>
+                    <a>LinkedIn</a>
+                </div>
+                <div className='sub-contact'>
+                    <a>Email</a>
+                </div>
+            </div>
+        </div>
+    </div>   
     )
 }
 
