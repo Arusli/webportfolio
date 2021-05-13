af9f70eba6d98a89d7eba6a2b9d1f5c8ff3939b2
Andrew Rusli <andreworusli@gmail.com>
2021-05-12 20:51:51 -0400

    changed web developer to grey

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
         <div className='landingpage centered-container'>
             <div className='title'>
                 <h1>andrew rusli</h1>
-                <h1>web developer</h1>
+                <h1 style={{color: 'grey', opacity: '.4'}}>web developer</h1>
             </div>
             <ul style={{marginTop: '20px', paddingTop: '10px', borderTop: 'solid 2px black'}}>
                 <li>miami tetris</li>
