a3a880a629f3ed13e53a3e8be1de2852210069d0
Andrew Rusli <andreworusli@gmail.com>
2021-05-13 18:46:03 -0400

    attempt reposition github icon w relative position

--- src/components/List.js
+++ src/components/List.js
@@ -9,13 +9,14 @@ const List = () => {
         <div id='list' className='bg-image centered-container' style={{backgroundImage: `url(${list})`, }}>
             <div className='inner-container centered-container'>
                 <div className='project-square listmaker'>
-                <a target="_blank" href="https://arusli-listmaker.netlify.app/"><h2 className='project-header'>List Maker</h2></a>
+                    <a target="_blank" href="https://arusli-listmaker.netlify.app/"><h2 className='project-header'>List Maker</h2></a>
                     <div className='inner-div'>
                         <p>A simple, fullstack, CRUD web app for making lists.</p>
                         <p>React/node/express/mysql.</p>  
                     </div>
+                    <div className="github-icon-container fixed-github-icon"><a target="_blank" href="https://github.com/Arusli/reactlistmakerclient"><AiFillGithub /></a></div>
                 </div>
-                <div className="github-icon-container fixed-github-icon"><a target="_blank" href="https://github.com/Arusli/reactlistmakerclient"><AiFillGithub /></a></div>
+                
             </div>
         </div>    
 
