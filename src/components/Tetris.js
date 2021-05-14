5ba24116585ae59ee833073b62b7c9dd9d40f03f
Andrew Rusli <andreworusli@gmail.com>
2021-05-13 20:51:43 -0400

    redesigned project square and github logo

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import tetris from '../images/tetris.png';
 import {AiFillGithub} from 'react-icons/ai';
+import {GoLogoGithub} from 'react-icons/go';
 import {Element} from 'react-scroll';
 
 const Tetris = () => {
@@ -9,12 +10,14 @@ const Tetris = () => {
             <div className='inner-container centered-container'>
                 
                 <div className='project-square tetris'>
-                <a target="_blank" href="https://miamitetris.herokuapp.com/"><h2 className='project-header'>Miami Tetris</h2></a>
                     <div className='inner-div'>
                         <p>A miami-themed version of the classic game.</p>
                         <p>Javascript/html/css.</p>
                     </div>
-                    <div className="github-icon-container fixed-github-icon"><a target="_blank" href="https://github.com/Arusli/miamitetris"><AiFillGithub /></a></div>
+                    
+                    <a target="_blank" href="https://miamitetris.herokuapp.com/"><h2 className='project-header'>Miami Tetris</h2></a>
+                    <div className="github-icon-container fixed-github-icon"><a target="_blank" href="https://github.com/Arusli/miamitetris"><GoLogoGithub /></a></div>
+                    
                 </div>
                 
                 
